0cfb2bcff50e588ddffbb17d2ab4b687839f10f0
Ryan Wido <rnwido@umich.edu>
2021-10-07 14:44:10 -0400

    Fixing syntax error.

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
   public static void main (String[] args){
     for (int i = 1; i <= 15; i++){
-      system.out.println("Hello World...0 +i");
+      System.out.println("Hello World...0 +i");
     }
   }
 }
